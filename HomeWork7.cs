6f55ecd2607c0855b128e01625cdaf510d05a9de
Artem <ar.toropov@mail.ru>
2022-12-26 22:54:01 +0300

    2 tasks are ready

--- HomeWork7.cs
+++ HomeWork7.cs
@@ -1,4 +1,4 @@
-﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿/*// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 int[,] Create2dAray(int rows, int colums)
 {
     int[,] array2d = new int[rows, colums];
@@ -28,6 +28,82 @@ int colums = Convert.ToInt32(Console.ReadLine());
 
 int[,] array2d = Create2dAray(rows, colums);
 Show2dArray(array2d);
-//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+*/
+
+/*//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+int[,] Create2dAray(int rows, int colums)
+{
+    int[,] array2d = new int[rows, colums];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < rows; j++)
+            array2d[i,j] = new Random(). Next(-100,100);
+
+    return array2d;
+}
+
+void Show2dArray(int[,] array2d)
+{
+    for(int i = 0; i < array2d.GetLength(0); i++)
+        {
+            for(int j = 0; j < array2d.GetLength(1); j++)
+            {
+                Console.Write (array2d[i,j] + "\t");
+            }
+            Console.WriteLine();  
+        }
+}
+
+void ShowElement(int[,] array2d, int row, int colum)
+{
+Console.WriteLine($"Element is {array2d[row - 1, colum - 1]}");
+}
+
+Console.WriteLine("input number of rows ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number of colums ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2d = Create2dAray(rows, colums);
+Show2dArray(array2d);
+
+Console.WriteLine("input number of row ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number of colum ");
+int colum = Convert.ToInt32(Console.ReadLine());
+if (row <= rows && row > 0 && colum <= colums && colum > 0)
+ShowElement(array2d, row, colum);
+else
+Console.WriteLine("Incorrect values");
+*/
 
 //Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] Create2dAray(int rows, int colums)
+{
+    int[,] array2d = new int[rows, colums];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < rows; j++)
+            array2d[i,j] = new Random(). Next(-100,100);
+
+    return array2d;
+}
+
+void Show2dArray(int[,] array2d)
+{
+    for(int i = 0; i < array2d.GetLength(0); i++)
+        {
+            for(int j = 0; j < array2d.GetLength(1); j++)
+            {
+                Console.Write (array2d[i,j] + "\t");
+            }
+            Console.WriteLine();  
+        }
+}
+
+Console.WriteLine("input number of rows ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number of colums ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2d = Create2dAray(rows, colums);
+Show2dArray(array2d);
+
